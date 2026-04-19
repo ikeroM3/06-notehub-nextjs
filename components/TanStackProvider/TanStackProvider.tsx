e8c51d1bb621344e77c00bd74ac13a6247b83dcd
Artem <stuzukartem12@gmail.com>
2026-04-19 16:54:22 +0300

    next.js

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import type { ReactNode } from "react";
 
-export default function Providers({ children }) {
+export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
